9c69ad3529982fdd69b54df14ff598db3a847406
sallecta <sallecta@inbox.ru>
2015-08-08 23:20:24 +0300

    Update rescanhw.cpp

--- rescanhw.cpp
+++ rescanhw.cpp
@@ -10,3 +10,18 @@ std::string to_string(int i)
 	ss << i;
 	return ss.str();
 }
+
+int wmain()
+{
+	std::wcout << "Starting scan for hardware changes..." << std::endl;
+	HMACHINE machineHandle = NULL;
+	DEVINST devRoot;
+	CM_Locate_DevNode_Ex(&devRoot,NULL,CM_LOCATE_DEVNODE_NORMAL,machineHandle);
+	DWORD result = CM_Reenumerate_DevNode_Ex(devRoot, CM_REENUMERATE_SYNCHRONOUS, machineHandle); 
+	if(result != CR_SUCCESS)
+	{
+		std::cout << "Scan incomplete. Error code:" + to_string(result);
+		ExitProcess(result);
+	}
+	std::cout << "Scan successfully complete.";
+ }
